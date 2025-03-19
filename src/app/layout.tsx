8863a7e4e1fb8e84ede7230c7a69fd43f9d6d5f2
alhassan50 <abdullahialhassanbaaba@gmail.com>
2025-03-19 17:13:05 +0000

    Add poppins font as fallback

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
+
 
 export const metadata: Metadata = {
   title: "Theresa Asamoah | Fashion Designer & Creative Visionary",
@@ -14,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-clash antialiased text-gray-600`}>
+      <body className={`${poppins.className} !font-clash antialiased text-gray-600`}>
         <div className="">{children}</div>
       </body>
     </html>
