b533d7fd8c8d8dd481ba5ef6bed8179334168762
alhassan50 <abdullahialhassanbaaba@gmail.com>
2025-03-19 13:33:00 +0000

    add line break between hero banner texts

--- src/app/home/components/Hero.tsx
+++ src/app/home/components/Hero.tsx
@@ -10,6 +10,7 @@ export default function Hero() {
         <h1 className="inline-block text-4xl bg-white py-2 px-8 rounded-full font-semibold text-gray-700 leading-tight">
           Hi, My Name is Theresa Asamoah
         </h1>
+        <br />
         <h1 className="bg-white py-2 px-8 inline-block rounded-full text-4xl font-semibold text-gray-700 leading-tight">
           A <span className="text-[var(--primary)]">Fashion Designer</span> &{" "}
           <span className="text-[var(--primary)]">Creative Visionary</span>
@@ -18,7 +19,10 @@ export default function Hero() {
 
       {/* Buttons */}
       <div className="grid md:flex gap-4 justify-center items-center">
-        <Link href={'/#project'} className="py-4 flex gap-1 items-center group text-[20px] px-8 font-medium rounded-full bg-[var(--primary)] text-[var(--background)] hover:bg-opacity-80 transition">
+        <Link
+          href={"/#project"}
+          className="py-4 flex gap-1 items-center group text-[20px] px-8 font-medium rounded-full bg-[var(--primary)] text-[var(--background)] hover:bg-opacity-80 transition"
+        >
           Go To Project
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -29,7 +33,10 @@ export default function Hero() {
             <path d="M16.0037 9.41421L7.39712 18.0208L5.98291 16.6066L14.5895 8H7.00373V6H18.0037V17H16.0037V9.41421Z"></path>
           </svg>
         </Link>
-        <Link href={'/#contact'} className="py-3 bg-white text-[20px] px-8 font-medium rounded-full hover:underline transition text-[var(--primary)] hover:bg-[#fafafa]">
+        <Link
+          href={"/#contact"}
+          className="py-3 bg-white text-[20px] px-8 font-medium rounded-full hover:underline transition text-[var(--primary)] hover:bg-[#fafafa]"
+        >
           Contact Me
         </Link>
       </div>
