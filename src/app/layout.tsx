0c686f902b97cd778d27c1b694e7216d642c02fa
alhassan50 <abdullahialhassanbaaba@gmail.com>
2025-03-19 13:37:06 +0000

    Update meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Theresa Asamoah",
+  title: "Theresa Asamoah | Fashion Designer & Creative Visionary",
   description:
     "This is the portfolio of Theresa Asamoah - a creative and detail-oriented professional showcasing expertise in design, development, and innovative digital experiences. Discover projects that blend aesthetics with functionality. Let's create something amazing together!",
 };
